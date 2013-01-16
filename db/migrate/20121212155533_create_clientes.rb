3342b0a33b9a700c83a970fc940a3f0b31c2c290
VictorMarques <rb1@rb-Infoway-a7420.(none)>
2013-01-16 15:54:44 -0200

    arrumado

--- db/migrate/20121212155533_create_clientes.rb
+++ db/migrate/20121212155533_create_clientes.rb
@@ -1,7 +1,7 @@
 class CreateClientes < ActiveRecord::Migration
   def change
     create_table :clientes do |t|
-    	t.strng :nome, limit: 80
+    	t.string :nome, limit: 80
     	t.string :idade
       t.timestamps
     end
